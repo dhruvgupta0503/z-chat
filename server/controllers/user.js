f8605fed53bc563811837b965d93b02aafa7c781
Dhruv Gupta <gdhruv579@gmail.com>
2024-05-27 14:31:24 +0530

    backend started

---
+++ server/controllers/user.js
@@ -0,0 +1,7 @@
+const login=(req,res)=>{
+    res.send("server runnning ")
+};
+
+export {
+    login
+};
---
+++ server/routes/user.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { login } from "../controllers/user.js";
+
+const app =express.Router();
+app.get("/login",login);
+
+export default app;
