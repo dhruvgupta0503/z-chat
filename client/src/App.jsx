09972a590bc6ed9a5c19f686ea5a0575eaf00582
amshal1 <panshal911@gmail.com>
2024-07-05 13:51:31 +0530

    client env file added in .gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,64 +1,70 @@
-import React, { Suspense, lazy, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import ProtectRoute from './components/auth/ProtectRoute';
-import { LayoutLoader } from './components/layout/Loaders';
-import axios from 'axios';
-import { server } from './constants/config';
-import { useDispatch, useSelector } from 'react-redux';
-import { userNotExists, userExists } from './redux/reducers/auth';
+import React, { Suspense, lazy, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
+import { LayoutLoader } from "./components/layout/Loaders";
+import axios from "axios";
+import { server } from "./constants/config";
+import { useDispatch, useSelector } from "react-redux";
+import { userNotExists, userExists } from "./redux/reducers/auth";
 import { Toaster } from "react-hot-toast";
 
-const Home = lazy(() => import('./pages/Home'));
-const Login = lazy(() => import('./pages/Login'));
-const Groups = lazy(() => import('./pages/Groups'));
-const Chat = lazy(() => import('./pages/Chat'));
-const AdminLogin = lazy(() => import('./pages/admin/AdminLogin'));
-const Dashboard = lazy(() => import('./pages/admin/Dashboard'));
-const UserManagement = lazy(() => import('./pages/admin/UserManagement'));
-const ChatManagement = lazy(() => import('./pages/admin/ChatManagement'));
-const MessagesManagement = lazy(() => import('./pages/admin/MessageManagement'));
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Groups = lazy(() => import("./pages/Groups"));
+const Chat = lazy(() => import("./pages/Chat"));
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+const UserManagement = lazy(() => import("./pages/admin/UserManagement"));
+const ChatManagement = lazy(() => import("./pages/admin/ChatManagement"));
+const MessagesManagement = lazy(() =>
+  import("./pages/admin/MessageManagement")
+);
 
 const App = () => {
-    const dispatch = useDispatch();
-    const { loader, user } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  const { loader, user } = useSelector((state) => state.auth);
 
-    useEffect(() => {
-        axios
-            .get(`${server}/api/v1/user/me`, { withCredentials: true })
-            .then(({ data }) => dispatch(userExists(data.user)))
-            .catch((err) => dispatch(userNotExists()));
-    }, [dispatch]);
+  useEffect(() => {
+    axios
+      .get(`${server}/api/v1/user/me`, { withCredentials: true })
+      .then(({ data }) => dispatch(userExists(data.user)))
+      .catch((err) => dispatch(userNotExists()));
+  }, [dispatch]);
 
-    return loader ? (
-        <LayoutLoader />
-    ) : (
-        <Router>
-            <Suspense fallback={<LayoutLoader />}>
-                <Routes>
-                    <Route path="/" element={<ProtectRoute user={user} redirect="/login" />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/groups" element={<Groups />} />
-                    <Route path="/chat/:chatId" element={<Chat />} />
-                    <Route path="/admin" element={<AdminLogin />} />
-                    <Route path="/admin/dashboard" element={<Dashboard />} />
-                    <Route path="/admin/users-management" element={<UserManagement />} />
-                    <Route path="/admin/chats-management" element={<ChatManagement />} />
-                    <Route path="/admin/messages" element={<MessagesManagement />} />
-                    <Route
-                        path="/login"
-                        element={
-                            user ? (
-                                <Navigate to="/home" replace />
-                            ) : (
-                                <Login />
-                            )
-                        }
-                    />
-                </Routes>
-            </Suspense>
-            <Toaster position='bottom-center' />
-        </Router>
-    );
+  console.log(user);
+
+  return loader ? (
+    <LayoutLoader />
+  ) : (
+    <Router>
+      <Suspense fallback={<LayoutLoader />}>
+        <Routes>
+          <Route
+            path="/"
+            element={<ProtectRoute user={user} redirect="/login" />}
+          />
+          <Route path="/home" element={<Home />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/users-management" element={<UserManagement />} />
+          <Route path="/admin/chats-management" element={<ChatManagement />} />
+          <Route path="/admin/messages" element={<MessagesManagement />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/home" replace /> : <Login />}
+          />
+        </Routes>
+      </Suspense>
+      <Toaster position="bottom-center" />
+    </Router>
+  );
 };
 
 export default App;
--- client/src/components/auth/ProtectRoute.jsx
+++ client/src/components/auth/ProtectRoute.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from "react-router-dom";
 
-const  ProtectRoute=(children,user,redirect="/login")=> {
-    if(!user) return <Navigate to ={redirect}/>
-    return children ? children:<Outlet/>; 
- 
-}
+const ProtectRoute = (children, user, redirect = "/login") => {
+  // use useNavigate instead of <Navigate/> in functional component
+  const navigate = useNavigate();
+  //   user is a object so if you use !user it will return false and the if condition will never run
+  if (!user || Object.keys(user).length === 0) return navigate(redirect);
+  //   if the above if condition dosen't run then the react will render children which includes {user: null, redired: "/login"} which is a object and you will get the error (you can log the children)
+  return children ? children : <Outlet />;
+};
 export default ProtectRoute;
