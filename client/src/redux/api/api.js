4d549c0cbc7ba561cca9f4cfc7dab5c653a1a1f7
Dhruv Gupta <gdhruv579@gmail.com>
2024-07-07 02:53:56 +0530

    issues resolved

--- client/src/redux/api/api.js
+++ client/src/redux/api/api.js
@@ -1,7 +1,7 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { server } from '../../constants/config';
-import { sendFriendRequest } from '../../../../server/controllers/user';
+
 //import { Chat } from '@mui/icons-material';
 const api= createApi({
     reducerPath: 'api',
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['mock-aws-s3', 'aws-sdk', 'nock', '@mapbox/node-pre-gyp'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['mock-aws-s3', 'aws-sdk', 'nock', '@mapbox/node-pre-gyp'],
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  plugins: [
-    {
-      name: 'html-loader',
-      enforce: 'pre',
-      transform(src, id) {
-        if (id.endsWith('.html')) {
-          return `export default ${JSON.stringify(src)}`;
-        }
-      },
-    },
-  ],
-});
+  plugins: [react()],
+})
